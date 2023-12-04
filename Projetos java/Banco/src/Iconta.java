5cf72d8a070826af9ff6cd7e8e1846bde7d1b169
ThielleSa <145806511+ThielleSa@users.noreply.github.com>
2023-12-04 15:12:14 -0300

    Delete Projetos java directory

--- Projetos java/Banco/src/Iconta.java
+++
@@ -1,8 +0,0 @@
-public interface Iconta {
-   
-    public abstract void sacar(double valorSaque);
-    public abstract void depositar(double valorDepositado);
-    public abstract void transferir(double valorTransferido);
-    public abstract void pagar(double valorDevido);
-    public static void cadastrar(){};
-}
--- Projetos java/Estrutura de dados/src/Generics/AppGenerics.java
+++
@@ -1,9 +0,0 @@
-package Generics;
-public class AppGenerics {
-    public static void main(String[] args) {
-        Tipo <String> lista = new Tipo <>();
-        lista.set("oi");
-
-    }
-    
-}
