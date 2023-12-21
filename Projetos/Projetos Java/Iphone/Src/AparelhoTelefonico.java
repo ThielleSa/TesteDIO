a446c6bbcf2ce1bd420c72a6b6030aed377ce418
ThielleSa <145806511+ThielleSa@users.noreply.github.com>
2023-12-21 08:59:40 -0300

    Add files via upload

---
+++ Projetos/Projetos Java/Iphone/Src/AparelhoTelefonico.java
@@ -0,0 +1,8 @@
+public interface AparelhoTelefonico {
+    public void ligar(int num);
+    public void atender();
+    public void iniciarCorreioVoz();
+    public void recusar();
+    public void encerrarChamada();
+    public void vivaVoz();
+}
